3a08e82d9e03eaf13631424a78d7b15a49ba6d33
İlknur Aşkar <ilknuraskar@codefirst.io>
2022-03-08 13:21:42 +0300

    cors error fixed

--- src/main/java/net/abidinozdurmaz/chess/controller/BoardController.java
+++ src/main/java/net/abidinozdurmaz/chess/controller/BoardController.java
@@ -13,11 +13,13 @@ public class BoardController {
 
     private final BoardRecord boardRecord;
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @PostMapping("/move")
     public ChessBoard move(@RequestBody ChessBoard chessBoard) {
         return boardRecord.move(chessBoard);
     }
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping("/start-board")
     public ChessBoard startBoard() {
         return new ChessBoard(ChessBoard.startingSequence(), Color.WHITE);
